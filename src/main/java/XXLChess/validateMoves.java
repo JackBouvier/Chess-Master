198c18c7c997561e1c4cd6bebc812406cf754ea5
jbou9902 <jbou9902@uni.sydney.edu.au>
2023-05-04 13:17:43 +1000

    big fix

--- src/main/java/XXLChess/validateMoves.java
+++ src/main/java/XXLChess/validateMoves.java
@@ -1,36 +1,17 @@
 package XXLChess;
 import java.util.ArrayList;
 
-/**
- * The validateMoves class is responsible for validating the possible moves
- * for a given chess piece on the board.
- */
 public class validateMoves {
-    
-    /**
-     * Validates the possible moves for a given chess piece based on the
-     * current state of the board and the piece's position.
-     * @param tempPiece   The chess piece whose possible moves are to be validated.
-     * @param validMoves  An ArrayList of chessTile objects representing the valid moves.
-     * @return An ArrayList of valid chessTile objects after validating the given piece's possible moves.
-     */
     public static ArrayList<chessTile> validate(chessPiece tempPiece, ArrayList<chessTile> validMoves){
         if (tempPiece != null) {
-            int whiteKingX = -1;
-            int whiteKingY = -1;
-            int blackKingX = -1;
-            int blackKingY = -1;
             ArrayList<chessTile> possibleMoves = tempPiece.getMoves(tempPiece.getPieceX(), tempPiece.getPieceY(), App.boardArray);
             for (chessTile move : possibleMoves) {
                 int moveX = move.getTileX();
                 int moveY = move.getTileY();
-                try{
-                    whiteKingX = king.whiteKing.getPieceX();
-                    whiteKingY = king.whiteKing.getPieceY();
-                    blackKingX = king.blackKing.getPieceX();
-                    blackKingY = king.blackKing.getPieceY();
-                }
-                catch(Exception e){}
+                int whiteKingX = king.whiteKing.getPieceX();
+                int whiteKingY = king.whiteKing.getPieceY();
+                int blackKingX = king.blackKing.getPieceX();
+                int blackKingY = king.blackKing.getPieceY();
                 @SuppressWarnings("unchecked")
                 Tuple<chessPiece, chessTile>[][] testArray2 = new Tuple[14][14];
                 for (int i = 0; i < 14; i++) {
@@ -72,17 +53,9 @@ public class validateMoves {
                         }
                     }
                 }
-                if (blackKingX != -1 && blackKingY != -1){
-                    if (!(whiteAttackTiles3.contains(App.boardArray[blackKingY][blackKingX].getTile()) && App.isWhiteTurn == false)){
-                        App.inCheck = false;
-                        validMoves.add(move);
-                    }
-                }
-                else if (whiteKingX != -1 && whiteKingY != -1){
-                    if (!(blackAttackTiles3.contains(App.boardArray[whiteKingY][whiteKingX].getTile()) && App.isWhiteTurn == true)){
-                        App.inCheck = false;
-                        validMoves.add(move);
-                    }
+                if (!(blackAttackTiles3.contains(App.boardArray[whiteKingY][whiteKingX].getTile()) && App.isWhiteTurn == true) && !(whiteAttackTiles3.contains(App.boardArray[blackKingY][blackKingX].getTile()) && App.isWhiteTurn == false)) {
+                    App.inCheck = false;
+                    validMoves.add(move);
                 }
             }
         } 
